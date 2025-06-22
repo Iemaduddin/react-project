31450e06ab3907df2472ae9934272f8b2ab8b117
Iemaduddin <93432453+Iemaduddin@users.noreply.github.com>
2025-06-23 00:37:20 +0700

    weather-app: added route with react-router-dom

--- weather-app/src/main.jsx
+++ weather-app/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import WeatherOpenMeteo from "./assets/components/pages/weather-openmateo";
+import WeatherOpenMap from "./assets/components/pages/weather-openweathermap";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <WeatherOpenMeteo />,
+  },
+  {
+    path: "/weather-openweathermap",
+    element: <WeatherOpenMap />,
+  },
+]);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
