a731b167527fa1382346234c2830d2880075d17a
Iemaduddin <93432453+Iemaduddin@users.noreply.github.com>
2025-07-24 18:19:12 +0700

    cms-app: add static file serving for public directory

--- cms-app/backend-cms-app/src/index.ts
+++ cms-app/backend-cms-app/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes";
+import path from "path";
 
 dotenv.config();
 const app = express();
@@ -9,10 +10,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(authRoutes);
+app.use(express.static("public"));
 // Route
 app.get("/", (_, res) => {
   res.send("CMS Backend is running");
 });
+
+// File Access
+app.use(express.static(path.join(__dirname, "public")));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
